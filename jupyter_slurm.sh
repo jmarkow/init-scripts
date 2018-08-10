689de0e92708693bffe2028e30b0d17c90b578e8
Jeffrey Markowitz <jeffrey_markowitz@hms.harvard.edu>
2018-08-10 11:33:11 -0400

    Updated

--- jupyter_slurm.sh
+++ jupyter_slurm.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=jupyter
-#SBATCH --output=jupyter_%j.log
-#SBATCH --partition=medium
-#SBATCH -n=10
-#SBATCH --mem=50GB
-#SBATCH --time=120:00:00
+#SBATCH --job-name jupyter
+#SBATCH --output jupyter_%j.log
+#SBATCH --partition medium
+#SBATCH -n 10
+#SBATCH --mem 50GB
+#SBATCH --time 100:00:00
 date;hostname;pwd
 
 # module add python
@@ -16,7 +16,10 @@ port=$(shuf -i 20000-30000 -n 1)
 echo -e "\nStarting Jupyter Notebook on port ${port} on the $(hostname) server."
 echo -e "\nSSH tunnel command: ssh -NL ${port}:localhost:${port} ${USER}@$(hostname)"
 echo -e "\nLocal URI: http://localhost:${port}"
+
+# source ${HOME}/.bashrc
 source activate moseq2
+cd ${HOME}
 jupyter notebook --no-browser --port=${port} --ip='*'
 
 date

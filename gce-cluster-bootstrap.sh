3d696bb88844d76a7445caa2fb78983771a7cde4
Jeffrey Markowitz <markowitzmeister@gmail.com>
2018-08-30 22:20:52 +0000

    Cleaned things up

--- gce-cluster-bootstrap.sh
+++ gce-cluster-bootstrap.sh
@@ -13,17 +13,17 @@ source activate moseq2
 
 conda install -c conda-forge ffmpeg
 
-ssh-keygen -t ras -b 4096 -C "markowitzmeister@gmail.com"
+ssh-keygen -t rsa -b 4096 -C "markowitzmeister@gmail.com"
 eval "$(ssh-agent -s)"
-ssh-add -K ~/.ssh/id_rsa
+ssh-add -k ~/.ssh/id_rsa
 
 read -p "Copy the ssh key to Github, press enter to continue..."
 
-pip install -e git@github.com:dattalab/moseq2-extract.git
-pip install -e git@github.com:dattalab/moseq2-pca.git
-pip install -e git@github.com:dattalab/moseq2-model.git --process-dependency-links
-pip install -e git@github.com:dattalab/moseq2-viz.git
-pip install -e git@github.com:dattalab/moseq2-batch.git
+pip install git+ssh://git@github.com/dattalab/moseq2-extract.git
+pip install git+ssh://git@github.com/dattalab/moseq2-pca.git
+pip install git+ssh://git@github.com/dattalab/moseq2-model.git --process-dependency-links
+pip install git+ssh://git@github.com/dattalab/moseq2-viz.git
+pip install git+ssh://git@github.com/dattalab/moseq2-batch.git
 
 git config --global user.email "markowitzmeister@gmail.com"
 git config --global user.name "Jeffrey Markowitz"

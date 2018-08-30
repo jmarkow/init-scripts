154d4d925ac4d2f03dd1feca0608f4aa56fe533f
jmarkow <jmarkow@cns.bu.edu>
2018-08-30 17:53:06 -0400

    Cleaned up gce bootstrap

--- gce-cluster-bootstrap.sh
+++ gce-cluster-bootstrap.sh
@@ -12,19 +12,18 @@ conda create -n "moseq2" python=3.6 -y
 source activate moseq2
 
 conda install -c conda-forge ffmpeg
-mkdir $HOME/python_repos/
 
-git clone https://jmarkow@github.com/dattalab/moseq2-extract.git $HOME/python_repos/moseq2-extract
-git clone https://jmarkow@github.com/dattalab/moseq2-pca.git $HOME/python_repos/moseq2-pca
-git clone https://jmarkow@github.com/dattalab/moseq2-model.git $HOME/python_repos/moseq2-model
-git clone https://jmarkow@github.com/dattalab/moseq2-viz.git $HOME/python_repos/moseq2-viz
-git clone https://jmarkow@github.com/dattalab/moseq2-batch.git $HOME/python_repos/moseq2-batch
+ssh-keygen -t ras -b 4096 -C "markowitzmeister@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add -K ~/.ssh/id_rsa
 
-pip install -e $HOME/python_repos/moseq2-extract
-pip install -e $HOME/python_repos/moseq2-pca
-pip install -e $HOME/python_repos/moseq2-model --process-dependency-links
-pip install -e $HOME/python_repos/moseq2-viz
-pip install -e $HOME/python_repos/moseq2-batch
+read -p "Copy the ssh key to Github, press enter to continue..."
+
+pip install -e git@github.com:dattalab/moseq2-extract.git
+pip install -e git@github.com:dattalab/moseq2-pca.git
+pip install -e git@github.com:dattalab/moseq2-model.git --process-dependency-links
+pip install -e git@github.com:dattalab/moseq2-viz.git
+pip install -e git@github.com:dattalab/moseq2-batch.git
 
 git config --global user.email "markowitzmeister@gmail.com"
 git config --global user.name "Jeffrey Markowitz"

1227569416ce7b63f26c63a8b39d0111e930e793
Jeffrey Markowitz <markowitzmeister@gmail.com>
2018-09-07 20:36:41 +0000

    Minor formatting update

--- jupyter_slurm_gce.sh
+++ jupyter_slurm_gce.sh
@@ -14,9 +14,9 @@ unset XDG_RUNTIME_DIR
 port=$(shuf -i 20000-30000 -n 1)
 
 echo -e "\nStarting Jupyter Notebook on port ${port} on the $(hostname) server."
-echo -e "\nSSH tunnel command: ssh -NL ${port}:localhost:${port} ${USER}@$(hostname)"
-echo -e "\nGCE tunnel command: gcloud compute ssh $(hostname) -- -NL ${port}:localhost:${port}"
-echo -e "\nLocal URI: http://localhost:${port}"
+echo -e "SSH tunnel command: ssh -NL ${port}:localhost:${port} ${USER}@$(hostname)"
+echo -e "GCE tunnel command: gcloud compute ssh $(hostname) -- -NL ${port}:localhost:${port}"
+echo -e "Local URI: http://localhost:${port}"
 
 # source ${HOME}/.bashrc
 source activate moseq2

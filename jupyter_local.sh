dfa2a6544cb2cec0b52bb5df8bad45ec496e786e
Jeffrey Markowitz <markowitzmeister@gmail.com>
2018-09-25 10:40:10 -0400

    Added gce to local

--- jupyter_local.sh
+++ jupyter_local.sh
@@ -2,6 +2,7 @@ port=$(shuf -i 20000-30000 -n 1)
 
 echo -e "\nStarting Jupyter Notebook on port ${port} on the $(hostname) server."
 echo -e "\nSSH tunnel command: ssh -NL ${port}:localhost:${port} ${USER}@$(hostname)"
+echo -e "GCE tunnel command: gcloud compute ssh $(hostname) -- -NL ${port}:localhost:${port}"
 echo -e "\nLocal URI: http://localhost:${port}"
 
 source activate moseq2

0fc11b8bbaf8b093651e02c71f1d8ad9cd42b32c
Jeffrey Markowitz <markowitzmeister@gmail.com>
2018-12-11 03:27:37 +0000

    Minor updates

--- gce-restore-nodes.sh
+++ gce-restore-nodes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PREEMPTED=( `/apps/slurm/current/bin/sinfo | grep down~ | sed -n 's/.*\(compute.*\).*/\1/p'` );
+PREEMPTED=( `/apps/slurm/current/bin/sinfo | grep down~ | sed -n 's/.* \(.*compute.*\).*/\1/p'` );
 
 if [[ $PREEMPTED ]]; then
 	/apps/slurm/current/bin/scontrol update nodename=$PREEMPTED state=idle

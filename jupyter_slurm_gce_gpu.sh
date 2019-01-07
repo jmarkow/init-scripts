c9c7152c52741905c4043543c031968b64c3f65c
Jeffrey Markowitz <markowitzmeister@gmail.com>
2019-01-07 16:28:35 +0000

    Added GPU jupyter stuff

---
+++ jupyter_slurm_gce_gpu.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#SBATCH --job-name jupyter
+#SBATCH --output jupyter_%j.log
+#SBATCH --partition debug
+#SBATCH -n 14
+#SBATCH --mem 50GB
+#SBATCH --time 120:00:00
+#SBATCH --gres gpu:2
+
+sudo su - jmarkow
+source /home/jmarkow/.bashrc
+date;hostname;pwd
+
+# module add python
+# export XDG_RUNTIME_DIR=${TMPDIR}
+unset XDG_RUNTIME_DIR
+
+port=$(shuf -i 20000-30000 -n 1)
+
+zone=( `gcloud compute instances list --filter="name=($(hostname))" --format='csv[no-heading](zone)'` )
+project=( `gcloud compute instances describe $(hostname) --zone=$zone | grep zone |  grep projects | sed 's/.*projects\/\([a-z|\-]*\)\/.*/\1/' | head -1`)
+
+echo -e "\nStarting Jupyter Notebook on port ${port} on the $(hostname) server."
+echo -e "SSH tunnel command: ssh -NL ${port}:localhost:${port} ${USER}@$(hostname)"
+echo -e "GCE tunnel command: gcloud compute --project=${project} ssh --zone=${zone} $(hostname) -- -NL ${port}:localhost:${port}"
+echo -e "Local URI: http://localhost:${port}"
+
+# source ${HOME}/.bashrc
+source activate moseq2
+cd ${HOME}
+jupyter notebook --no-browser --port=${port}
+
+date

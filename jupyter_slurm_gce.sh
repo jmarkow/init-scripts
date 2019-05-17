e030fd3be03fd7cf51409010fee7b8471cb4f4c5
Jeffrey Markowitz <markowitzmeister@gmail.com>
2019-05-17 08:23:03 -0700

    Added more external ip options

--- jupyter_slurm_gce.sh
+++ jupyter_slurm_gce.sh
@@ -15,15 +15,17 @@ port=$(shuf -i 20000-30000 -n 1)
 
 zone=( `gcloud compute instances list --filter="name=($(hostname))" --format='csv[no-heading](zone)'` )
 project=( `gcloud compute instances describe $(hostname) --zone=$zone | grep zone |  grep projects | sed 's/.*projects\/\([a-z|\-]*\)\/.*/\1/' | head -1`)
+external_ip=( `gcloud compute instances list | grep $(hostname) |  awk '{print $--NF}'` )
 
 echo -e "\nStarting Jupyter Notebook on port ${port} on the $(hostname) server."
 echo -e "SSH tunnel command: ssh -NL ${port}:localhost:${port} ${USER}@$(hostname)"
 echo -e "GCE tunnel command: gcloud compute --project=${project} ssh --zone=${zone} $(hostname) -- -NL ${port}:localhost:${port}"
 echo -e "Local URI: http://localhost:${port}"
+echo -e "Remote URI: http://${external_ip}:${port}"
 
 # source ${HOME}/.bashrc
 source activate moseq2
 cd ${HOME}
-jupyter notebook --no-browser --port=${port}
+jupyter notebook --no-browser --port=${port} --ip=0.0.0.0 --NotebookApp.custom_display_url="http://$external_ip:$port"
 
 date

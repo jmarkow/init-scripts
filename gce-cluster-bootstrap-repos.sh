fc49dbac8b15bfd51a70b5d8ef443f99b9d89c3e
Jeffrey Markowitz <markowitzmeister@gmail.com>
2019-08-05 08:27:38 -0400

    Minor changes

--- gce-cluster-bootstrap-repos.sh
+++ gce-cluster-bootstrap-repos.sh
@@ -1,6 +1,7 @@
-mkdir $HOME/dev
 git clone https://github.com/dattalab/usv-notebooks.git $HOME/dev/usv-notebooks
 git clone https://github.com/dattalab/mouse-rt-classifier.git $HOME/dev/mouse-rt-classifier
 git clone https://github.com/dattalab/voseq.git $HOME/dev/voseq
 git clone https://github.com/dattalab/modeling-notebooks.git $HOME/dev/modeling-notebooks
 git clone https://github.com/dattalab/reinforcement-analysis.git $HOME/dev/reinforcement-analyis
+git clone https://github.com/wingillis/dls-center-surround.git $HOME/dev/dls-center-surround
+git clone https://github.com/dattalab/scale-invariant-vae.git $HOME/dev/scale-invariant-vae
